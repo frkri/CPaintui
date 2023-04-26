1b8cd43b74f32a1ae2bcbb4d4cfb68d59f37afff
Wild <57220519+frkri@users.noreply.github.com>
2023-04-26 09:58:23 +0000

    Very basic test of ncurses

--- src/main.c
+++ src/main.c
@@ -1,7 +1,43 @@
+#include <curses.h>
+#include <ncurses.h>
 #include <stdio.h>
+#include <unistd.h>
+
+int display_modal(char *msg);
 
 int main(void) {
-  printf("Hello World!\n");
+  initscr(); // Initialize ncurses
+  cbreak();  // Stops buffering of Chars
+  noecho();  // Stops chars from being printed to the screen
+
+  printw("Hello World!\n");
+  refresh();
+  clear();
+
+  // Prepare Screen for next refresh
+  display_modal("Hello Again!");
+
+  getch(); // Wait for user input
+  refresh();
+
+  getch();
+
+  // End ncurses
+  endwin();
+
+  return 0;
+}
+
+int draw_base() { return 0; }
+
+int display_modal(char *msg) {
+
+  int Y = getmaxy(stdscr);
+  int X = getmaxx(stdscr);
+
+  printw("X: %i, Y: %i\n", X, Y);
+
+  mvprintw(15, 200, "MSG: %s\n", msg);
 
   return 0;
 }
